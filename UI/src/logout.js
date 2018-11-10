b31377e67dc50acec5fdd4d0e039b64f68a6dbc7
Jey <savalijey@gmail.com>
2018-11-11 02:52:40 +0300

    [Feature #161863106] Consume the logout endpoint

---
+++ UI/src/logout.js
@@ -0,0 +1,28 @@
+let logoutbtn = document.getElementById("logout");
+let logoutadmin = document.getElementById("logoutadmin");
+
+if (logoutbtn){
+logoutbtn.addEventListener("click", logoutFunc);
+}
+else{
+logoutadmin.addEventListener("click", logoutFunc);
+}
+
+function logoutFunc() {
+    fetch("https://store-manager-app.herokuapp.com/api/v2/auth/logout", {
+        method: 'POST',
+        headers: {
+            'x-access-token': localStorage.getItem("token")
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            alert(data.Message || data.message);
+            if (logoutadmin) {
+                window.location.replace("../index.html");
+            }
+            else {
+                window.location.replace("index.html");
+            }
+        })
+}
